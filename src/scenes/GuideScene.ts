5718354a93fefc443957cbe2953c2d1cec201673
Arthur Spessoto <artspessoto@gmail.com>
2026-01-15 04:15:01 -0300

    feat: create GuideScene to show modal

---
+++ src/scenes/GuideScene.ts
@@ -0,0 +1,67 @@
+import Phaser from "phaser";
+import { GUIDE_TEXT } from "../constants/Lore";
+import { ToonButton } from "../objects/ToonButton";
+
+export class GuideScene extends Phaser.Scene {
+  private text: string = `${GUIDE_TEXT.lore}\n\n${GUIDE_TEXT.rules}\n\n${GUIDE_TEXT.footer}`;
+
+  constructor() {
+    super("GuideScene");
+  }
+
+  create() {
+    const Pos_x = 640;
+    const height = 550;
+    const width = 800;
+    const x = (1280 - width) / 2;
+    const y = (720 - height) / 2;
+
+    this.add.rectangle(640, 360, 1280, 720, 0x000000, 0.7);
+
+    const panel = this.add.graphics();
+    // panel.fillStyle(0x222222, 1);
+    // panel.lineStyle(4, 0xffcc00, 1);
+    // panel.fillRoundedRect(340, 110, 600, 500, 20);
+    // panel.strokeRoundedRect(340, 110, 600, 500, 20);
+    panel.fillStyle(0x0a0a0a, 1);
+    panel.lineStyle(4, 0x996600, 1);
+  
+    //box
+    panel.fillRoundedRect(x, y, width, height, 20);
+    panel.strokeRoundedRect(x, y, width, height, 20);
+
+    this.add
+      .text(Pos_x, 160, GUIDE_TEXT.title, {
+        fontSize: "32px",
+        color: "#ffcc00",
+        fontStyle: "bold",
+        letterSpacing: 2,
+      })
+      .setOrigin(0.5);
+
+    this.add
+      .text(Pos_x, 360, this.text, {
+        fontSize: "1.4rem",
+        color: "#fff",
+        align: "center",
+        wordWrap: { width: 700 },
+      })
+      .setOrigin(0.5);
+
+    const closeBtn = new ToonButton(this, {
+      x: Pos_x,
+      y: 580,
+      text: "FECHAR",
+      width: 150,
+      height: 70,
+      color: 0x1a1a1a,
+      hoverColor: 0x2a2a2a,
+      textColor: "#fff",
+    });
+
+    closeBtn.on("pointerdown", () => {
+      this.scene.resume("MenuScene");
+      this.scene.stop();
+    });
+  }
+}
