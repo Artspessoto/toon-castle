13b7bc8cf06356c24763e62b65a10c96b6ffcd2d
Arthur Spessoto <artspessoto@gmail.com>
2026-01-17 01:25:41 -0300

    feat: create new interface to CardData and config to card position

---
+++ src/constants/Cards.ts
@@ -0,0 +1,11 @@
+export const CARD_CONFIG = {
+  WIDTH: 180,
+  HEIGHT: 250,
+  POSITIONS: {
+    NAME: { x: -75, y: -110 },
+    MANA: { x: 65, y: -100 },
+    DESC: { x: -75, y: 35 },
+    ATK: { x: -42, y: 92 },
+    DEF: { x: 35, y: 92 },
+  },
+};
---
+++ src/types/CardData.ts
@@ -0,0 +1,14 @@
+export type CardType = "MONSTER" | "EFFECT_MONSTER" | "SPELL" | "TRAP";
+
+export interface CardData {
+  id: string;
+  type: CardType;
+  nameKey: string;
+  atk?: number;
+  def?: number;
+  manaCost: number;
+  imageKey: string;
+  descriptionKey: string;
+  width?: number;
+  heigth?: number;
+}
