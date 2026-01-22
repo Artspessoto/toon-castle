90e878fd17c5a930102f8397260ab9000d65d819
Arthur Spessoto <artspessoto@gmail.com>
2026-01-22 03:15:23 -0300

    feat: add manager to input, to control drag and drop events

---
+++ src/managers/InputManager.ts
@@ -0,0 +1,108 @@
+import type { Card } from "../objects/Card";
+import type { BattleScene } from "../scenes/BattleScene";
+
+export class InputManager {
+  private scene: BattleScene;
+
+  constructor(scene: BattleScene) {
+    this.scene = scene;
+  }
+
+  public setupCardInteractions(card: Card) {
+    card.setInteractive({ draggable: true });
+    this.scene.input.setDraggable(card);
+
+    // hover effect (Zoom)
+    card.on("pointerover", () => this.handleCardHover(card));
+    card.on("pointerout", () => this.handleCardOut(card));
+
+    this.setupDragEvents(card);
+  }
+
+  private handleCardHover(card: Card) {
+    if (this.scene.gameState.isDragging) return;
+
+    this.scene.tweens.add({
+      targets: card.visualElements,
+      y: -280,
+      scale: 1.5,
+      duration: 200,
+      ease: "Back.easeOut",
+    });
+    card.setDepth(200);
+  }
+
+  private handleCardOut(card: Card) {
+    if (this.scene.gameState.isDragging) return;
+
+    this.scene.tweens.add({
+      targets: card.visualElements,
+      y: 0,
+      scale: 1,
+      duration: 200,
+      ease: "Power2",
+    });
+
+    this.scene.handManager.reorganizeHand();
+  }
+
+  public setupDragEvents(card: Card) {
+    card.on("dragstart", (pointer: Phaser.Input.Pointer) => {
+      if (this.scene.currentPhase !== "MAIN") {
+        this.scene.input.setDragState(pointer, 0);
+        return;
+      }
+
+      this.scene.gameState.setDragging(true);
+      this.scene.tweens.killTweensOf(card);
+      this.scene.tweens.killTweensOf(card.visualElements);
+
+      this.scene.tweens.add({
+        targets: card,
+        scale: 0.25,
+        duration: 150,
+        ease: "Power2",
+      });
+      card.setDepth(2000);
+    });
+
+    card.on("drag", (_pointer: any, dragX: number, dragY: number) => {
+      card.visualElements.setY(0);
+      card.visualElements.setScale(1);
+      card.setPosition(dragX, dragY);
+    });
+
+    card.on("dragend", (_pointer: any, dropped: boolean) => {
+      this.scene.gameState.setDragging(false);
+      if (!dropped) this.scene.handManager.reorganizeHand();
+      this.scene.tweens.add({
+        targets: card,
+        scale: 0.35,
+        duration: 200,
+        ease: "Back.easeOut",
+      });
+    });
+
+    card.on("drop", (_pointer: any, targetZone: Phaser.GameObjects.Zone) => {
+      const zoneType = targetZone.getData("type");
+      const cardType = card.getType();
+
+      const monsterCardValidation =
+        cardType.includes("MONSTER") && zoneType === "MONSTER";
+      const spellOrTrapCardValidation =
+        (cardType === "SPELL" || cardType === "TRAP") && zoneType === "SPELL";
+
+      const canPlay =
+        (monsterCardValidation || spellOrTrapCardValidation) &&
+        this.scene.currentPhase == "MAIN";
+
+      if (canPlay) {
+        this.scene.gameState.setDragging(false);
+        this.scene.handManager.removeCard(card);
+        this.scene.playCardOnFieldZone(card, targetZone);
+      } else {
+        this.scene.handManager.reorganizeHand();
+      }
+    });
+  }
+}
