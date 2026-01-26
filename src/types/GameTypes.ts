53c720ed1ac33136dab91fece8f7d06b2adb769a
Arthur Spessoto <artspessoto@gmail.com>
2026-01-26 00:44:03 -0300

    feat: update with new type to PlacementMode and Lang

--- src/types/GameTypes.ts
+++ src/types/GameTypes.ts
@@ -2,9 +2,11 @@ import { TRANSLATIONS } from "../constants/Translations";
 
 export type GamePhase = "DRAW" | "MAIN" | "BATTLE" | "ENEMY_TURN";
 export type CardType = "MONSTER" | "EFFECT_MONSTER" | "SPELL" | "TRAP";
+export type PlacementMode = "ATK" | "DEF" | "FACE_UP" | "SET";
 
 type Languages = keyof typeof TRANSLATIONS;
 type TranslationStructure = (typeof TRANSLATIONS)[Languages];
+export type Lang = "pt-br" | "en";
 export type BattleTranslations = TranslationStructure["battle_scene"];
 
 export interface CardData {
