a2aea667474069f45dd07e8f89b6b82fdab6819e
Arthur Spessoto <artspessoto@gmail.com>
2026-03-11 03:40:33 -0300

    feat: update EventBus with logger and typed event to emit and on case

--- src/events/EventBus.ts
+++ src/events/EventBus.ts
@@ -1,3 +1,32 @@
 import Phaser from "phaser";
+import type { GameEventMap } from "./GameEvents";
 
-export const EventBus = new Phaser.Events.EventEmitter();
+class TypedEventBus extends Phaser.Events.EventEmitter {
+  public emit<K extends keyof GameEventMap>(
+    event: K,
+    payload: GameEventMap[K],
+  ): boolean {
+    if (process.env.NODE_ENV == "development") {
+      console.log(
+        `%c[EVENT]%c ${event}%c`,
+        "color: #00ff00; font-weight: bold; background: #111; padding: 2px 5px; border-radius: 3px 0 0 3px;",
+        "color: #fff; background: #333; padding: 2px 5px; border-radius: 0 3px 3px 0;",
+        "color: #aaa; margin-left: 10px;",
+        payload,
+      );
+    }
+
+    return super.emit(event, payload);
+  }
+
+  public on<K extends keyof GameEventMap>(
+    event: K,
+    fn: (payload: GameEventMap[K]) => void,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    context?: any,
+  ): this {
+    return super.on(event, fn, context);
+  }
+}
+
+export const EventBus = new TypedEventBus();
